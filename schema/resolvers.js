8109396e92943fad9f367c6a259d853deeb2de63
smfilho <smfilho@gmail.com>
2021-10-21 16:33:31 -0500

    before mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,6 @@
-const { UserList, MovieList } = require('../FakeData');
+const { UserList, MovieList } = require("../FakeData");
+// not using DB, using lodash instead to query FakeData
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
@@ -7,14 +9,29 @@ const resolvers = {
       return UserList;
     },
     user: (parent, args) => {
-      const id = args.id;
+      const id = args.id;             //{id: id}
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
       // fetch data from an API here! Fetch or Axios maybe?
+    },
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name; //{ name: name }
+      const movie = _.find(MovieList, { name });
+      return movie;
     },
     /*
     ** First time I created this resolver, ran ok!
     friends() {
       return UserList;
     },*/
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2010 && movie.yearOfPublication <= 2020)
+    }
   },
 };
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -8,12 +8,22 @@ const typeDefs = gql`
     age: Int!
     nationality: Nationality!
     friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
     #creating query to retrieve info of one single user. Arguments!
     user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
 
     # First time creating a query, worked!
     #friends: [User]
